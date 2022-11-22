c7da2203fc1a173d499928527a2264fe7cc42ea8
Robin <robin.steil@gmx.de>
2022-11-22 10:30:40 +0100

    Removed venv files. Added requirements.txt

--- chatbots/barney/barney.py
+++ chatbots/barney/barney.py
@@ -1,7 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
-import time
 
 #build all urls
 # get all texts
@@ -39,6 +37,6 @@ for line_index, line in enumerate(lines):
         # wont work if previous line has no ":" or Barney has first line of an episode
         previous_line_text = previous_line[previous_line.find(":")+1:]
         question_and_answers[previous_line_text] = line[len("Barney:"):]
-        
+    
 for key, value in question_and_answers.items():
     print(key, value, sep="               ", end="\n---------------------")
