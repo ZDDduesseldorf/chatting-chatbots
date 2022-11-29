858d6cb08df4bb1837961f8b4de5bc4a9d4a9146
Sophie Raps <s.raps@onveda>
2022-11-29 15:05:05 +0100

    Added: config file

---
+++ chatbots/barney/config.py
@@ -0,0 +1,6 @@
+import os
+resources_directory_name = "resources"
+scraped_resources_folder_name = os.path.join(resources_directory_name, "scraped")
+processed_resources_folder_name = os.path.join(resources_directory_name, "processed")
+
+csv_separator = "---"
