4f6620159ca5e6373431656449e982cf3844ea99
Robin <robin.steil@gmx.de>
2022-12-09 11:27:45 +0100

    Skipp and track  small or empty processed files

--- chatbots/barney/barney.py
+++ chatbots/barney/barney.py
@@ -1,12 +1,21 @@
 import os
+import config
 
 questions_and_answers = {}
+ignored_episodes = []
 
 for file_name in os.listdir(config.processed_resources_folder_name):
     path = os.path.join(config.processed_resources_folder_name, file_name)
+    
+    if path.find("csv") == -1:
+        continue
+
+    
     with open(path, "r") as stream:
         lines = stream.read().splitlines()
-
+        if len(lines) < 15:
+            ignored_episodes.append(file_name)
+            continue
     for line_index, line in enumerate(lines):
         index = line.find("Barney:")
         if index == 0:
