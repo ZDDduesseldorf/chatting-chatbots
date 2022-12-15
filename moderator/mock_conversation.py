95f0c0192613114b4f281c31ec0a612054936461
philipp-vogel <philipp242@live.de>
2022-12-15 15:37:31 +0100

    Add mock convo and implement simularity check

---
+++ moderator/mock_conversation.py
@@ -0,0 +1,17 @@
+from moderator_bot import Message
+
+full_conversation = [
+    Message("Are you a fan of Google or Microsoft?", "1", "Freddy"),
+    Message("Both are excellent technology they are helpful in many ways. For the security purpose both are super.", "2", "Ben"),
+    Message("I'm not  a huge fan of Google, but I use it a lot because I have to. I think they are a monopoly in some sense.", "3", "Jefrey"),
+    Message("Google provides online related services and products, which includes online ads, search engine and cloud computing.", "4", "Tom"),
+    Message("Yeah, their services are good. I'm just not a fan of intrusive they can be on our personal lives.", "5", "Lisa"),
+    ]
+
+all_possible_message = [
+    Message("Google is leading the alphabet subsidiary and will continue to be the Umbrella company for Alphabet internet interest.", "1", "Freddy"),
+    Message("Sorry i don't get it.", "2", "Ben"),
+    Message("I like to dance every night.", "3", "Jefrey"),
+    Message("Some companies are good, some are bad.", "4", "Tom"),
+    Message("Ok, bye", "5", "Lisa")
+    ]
