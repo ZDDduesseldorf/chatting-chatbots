c1e8c0f958b1d5d91843810ab55854678673aba4
Robin <robin.steil@gmx.de>
2022-12-18 14:57:45 +0100

    Add function to replace entities

--- chatbots/barney/barney.py
+++ chatbots/barney/barney.py
@@ -1,10 +1,12 @@
 import csv
 import os
+import re
 from typing import Dict
 
 import config
 import numpy as np
 import pandas as pd
+import spacy
 from config import csv_quotechar, csv_separator
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -32,13 +34,31 @@ for file_name in os.listdir(config.processed_resources_folder_name):
                 continue
             corpus[prior_message] = barneys_message
 
+
+def replace_entity(message: str, new_entity: str) -> str:
+    nlp = spacy.load("en_core_web_lg")
+    nlp.add_pipe("merge_entities")
+    doc = nlp(message)
+    
+    response = message
+    ent_to_replace = None
+    for ent in doc.ents:
+        if ent.label_ == "PERSON":
+            ent_to_replace = ent
+            break
+
+    if(ent_to_replace):
+        response = re.sub(ent_to_replace.text, new_entity, message)
+    return response
+
 def respond(user_input: str) -> str:
     corpus_df = pd.DataFrame({"prior_message": list(corpus.keys()), "barneys_message": list(corpus.values())})
 
-    tfidf = TfidfVectorizer(min_df=2, max_df = 0.5, ngram_range=(1, 2))
+    tfidf = TfidfVectorizer(min_df = 2, max_df = 0.5, ngram_range = (1, 2))
     prior_messages_tfidf = tfidf.fit_transform(corpus_df.prior_message)
     input_tfidf = tfidf.transform([user_input])
 
     similarities = cosine_similarity(input_tfidf, prior_messages_tfidf)
     idx = np.argsort(similarities)[0][-1]
     return corpus_df.loc[idx, "barneys_message"]
+    
