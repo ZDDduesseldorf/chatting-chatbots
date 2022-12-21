3dca3840ab54db397309cfd5884ed26b8843dad3
Paul Kretschel <slither-fable0w@icloud.com>
2022-12-21 12:41:53 +0100

    Remove unused file

--- chatbots/transformer_chatbot/test.py
+++
@@ -1,7 +0,0 @@
-test = "c:/Users/User/Desktop/transformer/chatting-chatbots/chatting-chatbots/chatbots/transformer_chatbot/data/ed_all/500Samp_8192Vocab_25Length_64Batch_20000Buffer_2Layers_8Heads/2EPOCHS_weights_temp/part-00000-of-00001.data-00000-of-00001.tempstate16425950715168385226"
-
-count=0
-for i in test:
-    count += 1
-
-print(count)
