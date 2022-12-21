4edbdf8e60cab960046ca9d007f0005459db0185
Paul Kretschel <slither-fable0w@icloud.com>
2022-12-21 21:33:41 +0100

    Use activation param in model

--- chatbots/transformer_chatbot/transformer.py
+++ chatbots/transformer_chatbot/transformer.py
@@ -21,7 +21,7 @@ def encoder_layer(params: Params, name="encoder_layer"):
         epsilon=1e-6)(inputs + attention)
 
     outputs = tf.keras.layers.Dense(
-        units=params.num_units, activation='relu')(attention)
+        units=params.num_units, activation=params.activation)(attention)
     outputs = tf.keras.layers.Dense(units=params.d_model)(outputs)
     outputs = tf.keras.layers.Dropout(rate=params.dropout)(outputs)
     outputs = tf.keras.layers.LayerNormalization(
@@ -48,15 +48,16 @@ def encoder(params: Params, vocab_size: int, name="encoder"):
     return tf.keras.Model(inputs=[inputs, padding_mask], outputs=outputs, name=name)
 
 
-def decoder_layer(units, d_model, num_heads, dropout, name="decoder_layer"):
-    inputs = tf.keras.Input(shape=(None, d_model), name="inputs")
-    enc_outputs = tf.keras.Input(shape=(None, d_model), name="encoder_outputs")
+def decoder_layer(params: Params, name="decoder_layer"):
+    inputs = tf.keras.Input(shape=(None, params.d_model), name="inputs")
+    enc_outputs = tf.keras.Input(
+        shape=(None, params.d_model), name="encoder_outputs")
     look_ahead_mask = tf.keras.Input(
         shape=(1, None, None), name="look_ahead_mask")
     padding_mask = tf.keras.Input(shape=(1, 1, None), name='padding_mask')
 
     attention1 = multi_head_attention.MultiHeadAttention(
-        d_model, num_heads, name="attention_1")(inputs={
+        params.d_model, params.num_heads, name="attention_1")(inputs={
             'query': inputs,
             'key': inputs,
             'value': inputs,
@@ -66,19 +67,20 @@ def decoder_layer(units, d_model, num_heads, dropout, name="decoder_layer"):
         epsilon=1e-6)(attention1 + inputs)
 
     attention2 = multi_head_attention.MultiHeadAttention(
-        d_model, num_heads, name="attention_2")(inputs={
+        params.d_model, params.num_heads, name="attention_2")(inputs={
             'query': attention1,
             'key': enc_outputs,
             'value': enc_outputs,
             'mask': padding_mask
         })
-    attention2 = tf.keras.layers.Dropout(rate=dropout)(attention2)
+    attention2 = tf.keras.layers.Dropout(rate=params.dropout)(attention2)
     attention2 = tf.keras.layers.LayerNormalization(
         epsilon=1e-6)(attention2 + attention1)
 
-    outputs = tf.keras.layers.Dense(units=units, activation='relu')(attention2)
-    outputs = tf.keras.layers.Dense(units=d_model)(outputs)
-    outputs = tf.keras.layers.Dropout(rate=dropout)(outputs)
+    outputs = tf.keras.layers.Dense(
+        units=params.num_units, activation=params.activation)(attention2)
+    outputs = tf.keras.layers.Dense(units=params.d_model)(outputs)
+    outputs = tf.keras.layers.Dropout(rate=params.dropout)(outputs)
     outputs = tf.keras.layers.LayerNormalization(
         epsilon=1e-6)(outputs + attention2)
 
@@ -104,13 +106,8 @@ def decoder(params: Params, vocab_size: int, name='decoder'):
     outputs = tf.keras.layers.Dropout(rate=params.dropout)(embeddings)
 
     for i in range(params.num_layers):
-        outputs = decoder_layer(
-            units=params.num_units,
-            d_model=params.d_model,
-            num_heads=params.num_heads,
-            dropout=params.dropout,
-            name='decoder_layer_{}'.format(i),
-        )(inputs=[outputs, enc_outputs, look_ahead_mask, padding_mask])
+        outputs = decoder_layer(params, name='decoder_layer_{}'.format(i))(
+            inputs=[outputs, enc_outputs, look_ahead_mask, padding_mask])
 
     return tf.keras.Model(
         inputs=[inputs, enc_outputs, look_ahead_mask, padding_mask],
