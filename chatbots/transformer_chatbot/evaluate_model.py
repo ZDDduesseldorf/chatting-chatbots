2eff955073170dc0b85c399fd6eef0575a0abed2
Paul Kretschel <slither-fable0w@icloud.com>
2022-12-22 19:38:11 +0100

    Separate chatbot from model evaluation

--- chatbots/transformer_chatbot/evaluate_model.py
+++ chatbots/transformer_chatbot/evaluate_model.py
@@ -1,55 +1,19 @@
-import tensorflow as tf
 import helpers
 import data
 import transformer
+from chatbot import TransformerChatbot
 
 params = helpers.Params()
 loader = data.DataLoader(params)
-
 tokenizer = loader.get_tokenizer()
-START_TOKEN, END_TOKEN = [tokenizer.start_token], [tokenizer.end_token]
 
 model = transformer.transformer(params, tokenizer.vocab_size)
 model.load_weights(params.weights_path)
 
-
-def evaluate(sentence):
-    sentence = data.preprocess_sentence(sentence)
-
-    sentence = tf.expand_dims(
-        START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
-
-    output = tf.expand_dims(START_TOKEN, 0)
-
-    for _ in range(params.max_length):
-        predictions = model(inputs=[sentence, output], training=False)
-
-        # select the last word from the seq_len dimension
-        predictions = predictions[:, -1:, :]
-        predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
-
-        # return the result if the predicted_id is equal to the end token
-        if tf.equal(predicted_id, END_TOKEN[0]):
-            break
-
-        # concatenated the predicted_id to the output which is given to the decoder
-        # as its input.
-        output = tf.concat([output, predicted_id], axis=-1)
-
-    return tf.squeeze(output, axis=0)
-
-
-def predict(sentence):
-    prediction = evaluate(sentence)
-
-    predicted_sentence = tokenizer.decode(
-        [i for i in prediction if i < tokenizer.corpus_vocab_size])
-
-    return predicted_sentence
-
+chatbot = TransformerChatbot(model, tokenizer, params.max_length)
 
 while (True):
     inp = input('>> ')
     if inp in ["q", "quit", "exit"]:
         break
-    print(predict(inp))
+    print(chatbot.predict(inp))
