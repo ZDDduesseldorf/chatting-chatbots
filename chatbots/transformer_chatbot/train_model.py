a2b856a74f759fb9a8976e997e48e6094b38bcff
Paul Kretschel <slither-fable0w@icloud.com>
2022-12-22 17:13:08 +0100

    Improve loss function efficiency

--- chatbots/transformer_chatbot/train_model.py
+++ chatbots/transformer_chatbot/train_model.py
@@ -32,28 +32,26 @@ optimizer = tf.keras.optimizers.Adam(
     epsilon=1e-9)
 
 
+cross_entropy = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True,
+    reduction="none")
+
+
+def loss_function(y_true, y_pred):
+    y_true = tf.reshape(y_true, shape=(-1, params.max_length - 1))
+    loss = cross_entropy(y_true, y_pred)
+    mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
+    loss = tf.multiply(loss, mask)
+    return tf.reduce_mean(loss)
+
+
 def accuracy(y_true, y_pred):
     # ensure labels have shape (batch_size, MAX_LENGTH - 1)
     y_true = tf.reshape(y_true, shape=(-1, params.max_length - 1))
     return tf.keras.metrics.sparse_categorical_accuracy(y_true, y_pred)
 
 
-def loss_function(y_true, y_pred):
-    y_true = tf.reshape(y_true, shape=(-1, params.max_length - 1))
-
-    loss = tf.keras.losses.SparseCategoricalCrossentropy(
-        from_logits=True, reduction='none')(y_true, y_pred)
-
-    mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
-    loss = tf.multiply(loss, mask)
-
-    return tf.reduce_mean(loss)
-
-
-model.compile(
-    optimizer=optimizer,
-    loss=loss_function,
-    metrics=[accuracy])
+model.compile(optimizer=optimizer, loss=loss_function, metrics=[accuracy])
 
 train_dataset, val_dataset = loader.get_datasets()
 
