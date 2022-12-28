165c868085c4be4efbeffa025635cd8fe5026ac6
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2022-12-28 13:36:28 +0100

    Add moderator and sample chatbots

---
+++ moderator/samples_chatbots/chatbot2.py
@@ -0,0 +1,25 @@
+import random
+from typing import List
+
+from chatbotsclient.chatbot import Chatbot
+from chatbotsclient.message import Message
+
+
+def random_answer(message, conversation):
+    return random.choice(
+        [
+            "I like bread.",
+            "I love peanuts.",
+            "I prefer porridge.",
+            "Coconuts are my favorite.",
+        ]
+    )
+
+
+def respond(message: Message, conversation: List[Message]):
+    # custom answer computation of your chatbot
+    answer = random_answer(message.message, conversation)
+    return answer
+
+
+chatbot = Chatbot(respond, "Chatbot 2")
