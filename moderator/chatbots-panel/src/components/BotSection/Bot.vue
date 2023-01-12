6f3d44335ee025be6941cc6692d8e0319e6d06ad
Ben Kräling <ben.kraeling@study.hs-duesseldorf.de>
2023-01-12 22:04:59 +0100

    Add panel

---
+++ moderator/chatbots-panel/src/components/BotSection/Bot.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+export default {
+  setup() {},
+  props: {
+    name: "",
+    id: "",
+    voice: "",
+  },
+};
+</script>
+
+<template>
+  <div class="w-full flex flex-col my-2 py-1 px-2 rounded-lg bg-slate-700">
+    <b class="text-[12px] text-white">{{ name }}</b>
+    <small class="text-[12] text-white">{{ id }}</small>
+    <small class="text-[12] text-white">{{ voice }}</small>
+  </div>
+</template>
---
+++ moderator/chatbots-panel/src/components/ChatSection/Chat.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+import Conversation from "./Conversation.vue";
+import { usePusher } from "@/stores/pusher";
+import { useStore } from "@/stores/store";
+
+export default {
+  components: {
+    Conversation,
+  },
+  setup() {
+    const pusher = usePusher();
+    const store = useStore();
+    return { pusher, store };
+  },
+  data() {
+    return {
+      input: "",
+    };
+  },
+  methods: {
+    sendMessage() {
+      this.pusher.channel.trigger("client-provoke-message", this.input);
+      this.input = "";
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="flex flex-col h-full justify-between overflow-hidden">
+    <div class="overflow-hidden">
+      <Conversation />
+    </div>
+    <div class="pt-2 border-t border-teal-600">
+      <textarea
+        v-model="input"
+        class="w-full h-[80px] p-1 border rounded-lg resize-none outline-none text-cyan-900 border-teal-600 bg-transparent"
+        type="text"
+        placeholder="Message"
+      />
+      <button
+        @click="sendMessage"
+        class="absolute right-[5px] bottom-[10px] w-[80px] text-[12px] font-bold bg-slate-700 p-1 rounded-md text-white cursor-pointer"
+      >
+        Send
+      </button>
+    </div>
+  </div>
+</template>
