3d44f452c7c98aa6490aff428f29d1087d35b9fa
Robin <robin.steil@gmx.de>
2023-01-17 01:04:59 +0100

    add corpus path and write constants in caps

--- chatbots/barney/config.py
+++ chatbots/barney/config.py
@@ -1,8 +1,9 @@
 import os
 
-resources_directory_name = "resources"
-scraped_resources_folder_name = os.path.join(resources_directory_name, "scraped")
-processed_resources_folder_name = os.path.join(resources_directory_name, "processed")
+RESOURCES_DIRECTORY_NAME = "resources"
+scraped_resources_folder_name = os.path.join(RESOURCES_DIRECTORY_NAME, "scraped")
+processed_resources_folder_name = os.path.join(RESOURCES_DIRECTORY_NAME, "processed")
+corpus_path = os.path.join(processed_resources_folder_name, "corpus.csv")
 
-csv_separator = ","
-csv_quotechar = "\""
+CSV_SEPERATOR = ","
+CSV_QUOTECHAR = '"'
